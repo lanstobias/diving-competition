178516bf6143a11e85a2bf4f759395bd33a5bd74
BoboLillqvist <bobo.lillqvist@gmail.com>
2018-02-02 12:40:34 +0100

    Skapa projekt
    C# start av projekt i visual med både simhopp project och testsimhopp
    project

---
+++ testsimhopp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testsimhopp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testsimhopp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a296fe6-8aa0-44e7-b97f-55e51c1d4e0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
