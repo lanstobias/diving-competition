75b705d14ad68a04f317ac81a73de037ca7fb783
OSandh <oskarsandh@gmail.com>
2018-02-09 16:08:30 +0100

    Lägg till MainMenuPresenter

---
+++ simhopp/Presenters/MainMenuPresenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simhopp
+{
+    class MainMenuPresenter
+    {
+        public IMainMenuView View { get; set; }
+
+        private Form window;
+
+        #region Constructor
+        public MainMenuPresenter(IMainMenuView view, Form window)
+        {
+            this.View = view;
+            this.window = window;
+            View.EventCreateNewContest += gotoCreateContestView;
+        }
+        #endregion
+        
+        public void gotoCreateContestView()
+        {
+            //CreateContestView view = new Crateasdfasfd
+            //CreateContestPresenter presenter = new asdfasdf
+            //window.ChangePanel(view, this)   // ChangePanel(ny view, cameFrom)
+        }
+    }
+}
