0f50aa33fb6e572de123aadb719ac78a61ef8113
Tobias Lans <lanstobias@gmail.com>
2018-02-09 14:15:31 +0100

    Skapa test för att koppla upp till databas

--- testsimhopp/DatabaseConnectionTests.cs
+++ testsimhopp/DatabaseConnectionTests.cs
@@ -12,5 +12,22 @@ namespace Simhopp.Tests
     [TestClass()]
     public class DatabaseConnectionTests
     {
+        private MySqlConnection connection;
+
+        [TestMethod()]
+        public void TestDatabaseConnection()
+        {
+            MySqlConnectionStringBuilder connectionString = new MySqlConnectionStringBuilder();
+            connectionString.Server = "tomat.trickip.net";
+            connectionString.UserID = "root";
+            connectionString.Password = "gallian0";
+            connectionString.Database = "simhopp";
+
+            connection = new MySqlConnection(connectionString.ToString());
+
+            // If a connection can not open, an exception will be thrown
+            // and the test will fail.
+            connection.Open();
+        }
     }
 }
