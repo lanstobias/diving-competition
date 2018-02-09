91c57a30e614f763ff76659567d7f1bea2ce266a
Tobias Lans <lanstobias@gmail.com>
2018-02-09 13:23:41 +0100

    Skapa DatabaseConnection klassen

---
+++ simhopp/DatabaseConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Simhopp
+{
+    class DatabaseConnection
+    {
+        #region Fields
+        private MySqlConnection connection;
+
+        private string server;
+        private string database;
+        private string userID;
+        private string password;
+        #endregion
+
+        #region Constructor
+        public DatabaseConnection()
+        {
+            server = "localhost";
+            database = "connectcsharptomysql";
+            userID = "username";
+            password = "gallian0";
+
+            InitializeConnection();
+        }
+        #endregion
+
+    }
+}
