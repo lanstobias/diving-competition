1f92b851e0062cee4ba697ad321de8ddf7bca5b4
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-02-09 12:39:30 +0100

    några klassserrr

---
+++ simhopp/models/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simhopp.models
+{
+    public class Person
+    {
+
+        #region Properties
+        public int ID { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public int Age { get; set; } 
+
+        public String Email { get; set; }
+
+        public String Gender { get; set; }
+
+        public String SocialSecurityNr { get; set; }
+
+        public String Address { get; set; }
+        #endregion
+
+        public Person()
+        {
+
+        }
+
+
+
+    }
+}
