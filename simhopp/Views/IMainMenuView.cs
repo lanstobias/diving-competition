aa7d63a4f50dd69462e068141d89ffc1dc11b484
OSandh <oskarsandh@gmail.com>
2018-02-09 16:07:43 +0100

    Lägg till IMainMenu

---
+++ simhopp/Views/IMainMenuView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    #region Delegates
+    public delegate void DelegateCreateNewContest();
+    #endregion
+    interface IMainMenuView
+    {
+        event DelegateCreateNewContest EventCreateNewContest;
+    }
+}
