1ba78f7deadc4f389a149eb964d75a521db1de42
Tobias Lans <lanstobias@gmail.com>
2018-02-09 14:14:36 +0100

    Skapa DatabaseConnectionTests

---
+++ testsimhopp/DatabaseConnectionTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using Simhopp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp.Tests
+{
+    [TestClass()]
+    public class DatabaseConnectionTests
+    {
+    }
+}
