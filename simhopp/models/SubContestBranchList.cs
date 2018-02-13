4a4d46d22462ffe13e9c82e9665e7c75918e6243
BoboLillqvist <bobo.lillqvist@gmail.com>
2018-02-13 15:22:03 +0100

    Lägg till SubContestBranchList klassen

---
+++ simhopp/models/SubContestBranchList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    class SubContestBranchList : List<Score>
+    {
+
+        #region Constructor(s)
+
+        public SubContestBranchList()
+            : base()
+        {
+
+        }
+        #endregion
+
+
+    }
+}
