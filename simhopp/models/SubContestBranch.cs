a3e47adc459341de99b449ff0222c0bc5fa52f8d
BoboLillqvist <bobo.lillqvist@gmail.com>
2018-02-13 15:17:53 +0100

    Skapa SubContestBranch klassen
    Detta är en contests gren. Vi ville inte använda namnet Event eller
    DiveEvent för att undvika potentiell förviring i koden.

---
+++ simhopp/models/SubContestBranch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Denna klass är en Gren, vi valde namnet SubContestBranch eftersom namnet Event eller DiveEvent skulle kunna skapa förviring. Detta namn förvirar kanske också, men inte lika mycket ansåg vi.
+namespace Simhopp
+{
+    class SubContestBranch
+    {
+
+        public SubContestBranch()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public Contest ParentContest { get; set; }
+    }
+}
