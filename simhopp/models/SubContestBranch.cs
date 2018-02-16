b8a04eb08309f3a9299a808185560eda74e01e78
OSandh <oskarsandh@gmail.com>
2018-02-16 13:59:34 +0100

    Uppdatera AddNewDive funktionen

--- simhopp/models/SubContestBranch.cs
+++ simhopp/models/SubContestBranch.cs
@@ -33,40 +33,41 @@ namespace Simhopp
         /// <summary>
         /// Add a new dive to the correct divelist of a Contestant
         /// </summary>
-        /// <param name="Diver">The Contestant who made the dive</param>
-        /// <param name="DiveToBeAdded">The dive</param>
-        public void AddNewDive(Contestant Diver, Dive DiveToBeAdded)
+        /// <param name="diver">The Contestant who made the dive</param>
+        /// <param name="diveToBeAdded">The dive</param>
+        public void AddNewDive(Contestant diver, Dive diveToBeAdded)
         {
-            if (this.BranchContestants.Contains(Diver))
+            if (this.BranchContestants.Contains(diver))
             {
 
                 // If this is the first dive of this Contestant, create a new DiveList connected to this branch,
                 // and then add the dive.
-                if (Diver.DiveLists.Count == 0)
+                if (diver.DiveLists.Count == 0)
                 {
-                    Diver.DiveLists.Add(new DiveList(this));
-                    Diver.DiveLists.ElementAt(0).Add(DiveToBeAdded);
+                    diver.DiveLists.Add(new DiveList(this));
+                    diver.DiveLists.ElementAt(0).Add(diveToBeAdded);
                 }
                 // if it is not the first dive, go through the DiveList(s) and see if any of them match with
                 // this branch. If no such list exists, create it and add the dive.
                 else
                 {
-                    foreach (var list in Diver.DiveLists)
+                    foreach (var list in diver.DiveLists)
                     {
                         if (list.SubContestBranch == this)
                         {
-                            list.Add(DiveToBeAdded);
+                            list.Add(diveToBeAdded);
                             break;
                         }
                         else
                         {
-                            Diver.DiveLists.Add(new DiveList(this));
-                            Diver.DiveLists.Last().Add(DiveToBeAdded);
+                            diver.DiveLists.Add(new DiveList(this));
+                            diver.DiveLists.Last().Add(diveToBeAdded);
                             break;
                         }
                     }
                 }
             }
+        }
             
         }
         #endregion
