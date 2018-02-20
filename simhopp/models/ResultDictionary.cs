a9e2a76963fef17c0b05a02aa5a7bb7bed717e99
OSandh <oskarsandh@gmail.com>
2018-02-20 12:59:46 +0100

    Skapa klassen ResultDictionary
    ResultDictionary används för att hantera par av deltagare och deras totala poäng för en contest/subcontest.
    Ärver från Dictionary<Contestant, double>

---
+++ simhopp/models/ResultDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    /// <summary>
+    /// This class inherits from Dictionary to store pairs of Contestants and their total score for a certain contest or subContest.
+    /// </summary>
+    public class ResultDictionary : Dictionary<Contestant, double>
+    {
+        public void Sort()
+        {
+            var sortedDict = this.OrderByDescending(x => x.Value).ToList();
+
+            this.Clear();
+
+            foreach (var val in sortedDict)
+                this.Add(val.Key, val.Value);
+            
+        }
+    }
+}
