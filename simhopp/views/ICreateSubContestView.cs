58577e669a2cc46c85a3b20b8710822c286c7071
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-02-23 15:10:21 +0100

    Lägg till Interface ICreateSubContestView

---
+++ simhopp/views/ICreateSubContestView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public delegate void DelegateAddContestantToSubContest();
+    public delegate void DelegateRemoveContestantFromSubContest();
+    public delegate void DelegateAddSubContest();
+    public delegate void DelegateFinalizeContest();
+
+    public interface ICreateSubContestView
+    {
+        event DelegateAddContestantToSubContest EventAddContestantToSubContest;
+        event DelegateRemoveContestantFromSubContest EventRemoveContestantFromSubContest;
+        event DelegateAddSubContest EventAddSubContest;
+        event DelegateFinalizeContest EventFinalizeContest;
+
+    }
+}
