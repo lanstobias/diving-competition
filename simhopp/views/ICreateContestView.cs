24b498be2a33f321cbca569a88ae096c9e51e177
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-02-23 15:09:39 +0100

    Lägg till Interface ICreateContestView

---
+++ simhopp/views/ICreateContestView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public delegate void DelegateSetStartDate();
+    public delegate void DelegateSetEndDate();
+    public delegate void DelegateAddJudgeToDB();
+    public delegate void DelegateAddJudgeToContest();
+    public delegate void DelegateRemoveJudgeFromContest();
+    public delegate void DelegateAddContestantToDB();
+    public delegate void DelegateAddContestantToContest();
+    public delegate void DelegateRemoveContestantFromContest();
+    public delegate void DelegateCreateSubContest();
+
+    public interface ICreateContestView
+    {
+        event DelegateSetStartDate EventSetStartDate;
+        event DelegateSetEndDate EventSetEndDate;
+        event DelegateAddJudgeToDB EventAddJudgeToDB;
+        event DelegateAddJudgeToContest EventAddJudgeToContest;
+        event DelegateRemoveJudgeFromContest EventRemoveJudgeFromContest;
+        event DelegateAddContestantToDB EventAddContestantToDB;
+        event DelegateAddContestantToContest EventAddContestantToContest;
+        event DelegateRemoveContestantFromContest EventRemoveContestantFromContest;
+        event DelegateCreateSubContest EventCreateSubContest;
+
+    }
+}
