dc6d11f4a94422e6510593582b8c12174f893747
Tobias Lans <lanstobias@gmail.com>
2018-02-28 16:11:48 +0100

    Skapa Database.StorePerson()

--- simhopp/Database.cs
+++ simhopp/Database.cs
@@ -15,5 +15,29 @@ namespace Simhopp
         {
         }
 
+        public void StorePerson(Person person)
+        {
+            // Table info
+            string table = "person";
+
+            // Person info
+            var id = person.ID;
+            var firstName = person.FirstName;
+            var lastName = person.LastName;
+            var age = person.Age;
+            var email = person.Email;
+            var gender = person.Gender;
+            var socialSecurityNr = person.SocialSecurityNr;
+            var address = person.Address;
+
+            // Build query
+            string query = $"INSERT INTO {table} ";
+            query += $"(id, firstName, lastName, age, email, gender, socialSecurityNr, address) ";
+            query += $"VALUES(";
+            query += $"'{id}','{firstName}','{lastName}',{age},'{email}','{gender}',{socialSecurityNr},'{address}'";
+            query += $")";
+
+            ExecuteQuery(query);
+        }
     }
 }
