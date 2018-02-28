2bc92b9a65cdf7c6aa4519ab6425c2e2a2ece493
Tobias Lans <lanstobias@gmail.com>
2018-02-28 16:11:17 +0100

    Skapa klass Database

---
+++ simhopp/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Simhopp
+{
+    public class Database
+    {
+        private DatabaseConnection DBConnection = new DatabaseConnection();
+
+        public Database()
+        {
+        }
+
+    }
+}
