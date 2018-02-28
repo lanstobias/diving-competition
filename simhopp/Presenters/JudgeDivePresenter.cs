6585aa3c6ab404cdb274fd7ba9debaaa434f6500
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-02-28 13:37:17 +0100

    Skapa presentern JudgeDivePresenter

---
+++ simhopp/Presenters/JudgeDivePresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public class JudgeDivePresenter
+    {
+        public JudgeDiveView View { get; set; }
+
+        private ProjectMainWindow window;
+
+        public JudgeDivePresenter(JudgeDiveView view, ProjectMainWindow window)
+        {
+            this.View = view;
+            this.window = window;
+
+            View.EventGiveScore += GiveScore;
+        }
+        private void GiveScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
