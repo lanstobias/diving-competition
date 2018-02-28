7721a1e17ed635296be9bd3c0dc47b4fa2d2e310
Tobias Lans <lanstobias@gmail.com>
2018-02-28 16:14:58 +0100

    Skapa DatabasePerson klass i DatabaseSpike

---
+++ DatabaseSpikes/DatabasePerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Simhopp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSpikes
+{
+    public class DatabasePerson
+    {
+        public DatabasePerson()
+        {
+
+        }
+    }
+}
