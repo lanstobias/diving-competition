db83992239ccabb2ce45e42c0f58bece593397c6
OSandh <oskarsandh@gmail.com>
2018-03-01 18:36:22 +0100

    Lägg till formen AddPersonForm
    Simpelt fönster för att samla in information om en ny person som ska in till databasen.

---
+++ simhopp/AddPersonForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Simhopp
+{
+    partial class AddPersonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxSSN = new System.Windows.Forms.TextBox();
+            this.labelSSN = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.buttonSaveAndClose = new System.Windows.Forms.Button();
+            this.buttonSaveAndNew = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(12, 9);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(48, 13);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "Förnamn";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(66, 6);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(130, 20);
+            this.textBoxFirstName.TabIndex = 1;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(66, 32);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(130, 20);
+            this.textBoxLastName.TabIndex = 3;
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(12, 35);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(55, 13);
+            this.labelLastName.TabIndex = 2;
+            this.labelLastName.Text = "Efternamn";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(66, 58);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(130, 20);
+            this.textBoxAge.TabIndex = 5;
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(12, 61);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(31, 13);
+            this.labelAge.TabIndex = 4;
+            this.labelAge.Text = "Ålder";
+            // 
+            // textBoxSSN
+            // 
+            this.textBoxSSN.Location = new System.Drawing.Point(66, 137);
+            this.textBoxSSN.Name = "textBoxSSN";
+            this.textBoxSSN.Size = new System.Drawing.Size(130, 20);
+            this.textBoxSSN.TabIndex = 11;
+            // 
+            // labelSSN
+            // 
+            this.labelSSN.AutoSize = true;
+            this.labelSSN.Location = new System.Drawing.Point(12, 140);
+            this.labelSSN.Name = "labelSSN";
+            this.labelSSN.Size = new System.Drawing.Size(52, 13);
+            this.labelSSN.TabIndex = 10;
+            this.labelSSN.Text = "Personnr.";
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(12, 113);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(26, 13);
+            this.labelGender.TabIndex = 8;
+            this.labelGender.Text = "Kön";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(66, 84);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(130, 20);
+            this.textBoxEmail.TabIndex = 7;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(12, 87);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(34, 13);
+            this.labelEmail.TabIndex = 6;
+            this.labelEmail.Text = "Epost";
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Location = new System.Drawing.Point(66, 110);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(130, 21);
+            this.comboBoxGender.TabIndex = 12;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(66, 163);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(130, 20);
+            this.textBoxAddress.TabIndex = 14;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(12, 166);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(39, 13);
+            this.labelAddress.TabIndex = 13;
+            this.labelAddress.Text = "Adress";
+            // 
+            // buttonSaveAndClose
+            // 
+            this.buttonSaveAndClose.Location = new System.Drawing.Point(66, 189);
+            this.buttonSaveAndClose.Name = "buttonSaveAndClose";
+            this.buttonSaveAndClose.Size = new System.Drawing.Size(130, 24);
+            this.buttonSaveAndClose.TabIndex = 15;
+            this.buttonSaveAndClose.Text = "Spara och stäng";
+            this.buttonSaveAndClose.UseVisualStyleBackColor = true;
+            this.buttonSaveAndClose.Click += new System.EventHandler(this.buttonSaveAndClose_Click);
+            // 
+            // buttonSaveAndNew
+            // 
+            this.buttonSaveAndNew.Location = new System.Drawing.Point(66, 219);
+            this.buttonSaveAndNew.Name = "buttonSaveAndNew";
+            this.buttonSaveAndNew.Size = new System.Drawing.Size(130, 24);
+            this.buttonSaveAndNew.TabIndex = 16;
+            this.buttonSaveAndNew.Text = "Spara och skapa ny";
+            this.buttonSaveAndNew.UseVisualStyleBackColor = true;
+            this.buttonSaveAndNew.Click += new System.EventHandler(this.buttonSaveAndNew_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(66, 249);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(130, 24);
+            this.buttonClose.TabIndex = 17;
+            this.buttonClose.Text = "Stäng";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // AddPersonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 280);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSaveAndNew);
+            this.Controls.Add(this.buttonSaveAndClose);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.textBoxSSN);
+            this.Controls.Add(this.labelSSN);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelFirstName);
+            this.Name = "AddPersonForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxSSN;
+        private System.Windows.Forms.Label labelSSN;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Button buttonSaveAndClose;
+        private System.Windows.Forms.Button buttonSaveAndNew;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
