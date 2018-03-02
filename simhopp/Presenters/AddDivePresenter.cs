b4f420a7ff553ee72c945138f4e56654d54085c9
OSandh <oskarsandh@gmail.com>
2018-03-02 10:11:39 +0100

    Skapa AddDIvePresenter

---
+++ simhopp/Presenters/AddDivePresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public class AddDivePresenter
+    {
+        public AddDiveView View { get; set; }
+
+        private ProjectMainWindow window;
+
+        public Contest CurrentContest { get; set; }
+
+        public AddDivePresenter(AddDiveView view, ProjectMainWindow window, Contest contest)
+        {
+            this.View = view;
+            this.window = window;
+            CurrentContest = contest;
+        }
+    }
+}
