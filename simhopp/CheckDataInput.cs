8313aa269e6191ab783cc80675f16bc88aae1313
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-03-03 21:34:10 +0100

    Skapa CheckDataInput klass

---
+++ simhopp/CheckDataInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public static class CheckDataInput
+    {
+        // check if a string is formatted correctly
+        public static bool StringCheckFormat(string s)
+        {
+            Regex regex = new Regex(@"^[a-öA-Ö0-9_ -]+$");
+
+            if (regex.IsMatch(s))
+                return true;
+
+            return false;
+        }
+
+    }
+}
