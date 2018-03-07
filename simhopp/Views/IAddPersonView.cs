f8614d5aa209c4f878b00924a64324dfde4098a1
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-03-07 16:05:25 +0100

    Skapa AddPersonView

---
+++ simhopp/Views/IAddPersonView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public delegate void DelegateSaveAndNew();
+    public delegate void DelegateSaveAndClose();
+
+    public interface IAddPersonView
+    {
+        event DelegateSaveAndNew EventSaveAndNew;
+        event DelegateSaveAndClose EventSaveAndClose;
+
+    }
+}
