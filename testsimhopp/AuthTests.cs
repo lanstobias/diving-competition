bd12f71d3a45591733d6e8bb56a6aedeabf45272
Tobias Lans <lanstobias@gmail.com>
2018-03-12 13:29:53 +0100

    Skapa AuthTest.PasswordsMatchTest()

--- testsimhopp/AuthTests.cs
+++ testsimhopp/AuthTests.cs
@@ -13,5 +13,16 @@ namespace Simhopp.Tests
     [TestClass()]
     public class AuthTests
     {
+        [TestMethod()]
+        public void PasswordsMatchTest()
+        {
+            string inputPassword = "these pretzels are making me thirsty";
+            string expectedHash = "f98c73c790e7b4e571b413f39330b506748823ae";
+
+            string hashedInputPassword = Helpers.SHA1Hash(inputPassword);
+
+            Auth auth = new Auth();
+            Assert.IsTrue(auth.PasswordsMatch(expectedHash, hashedInputPassword));
+        }
     }
 }
