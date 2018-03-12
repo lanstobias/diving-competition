73e19c6d58c2eb647499d641795844f93d8882c9
Tobias Lans <lanstobias@gmail.com>
2018-03-12 15:17:48 +0100

    Uppdatera Program i DatabaseSpikes

--- DatabaseSpikes/Program.cs
+++ DatabaseSpikes/Program.cs
@@ -29,6 +29,22 @@ namespace DatabaseSpikes
                 Console.Write(person.ID + ": " + person.GetFullName() + " " + person.Age + "\n");
             }
 
+            List<Judge> judges = databasePerson.FetchJudges();
+
+            Console.WriteLine("\nSkriv ut domare:");
+            foreach (var judge in judges)
+            {
+                Console.Write(judge.ID + ": " + judge.GetFullName() + " " + judge.Age + "\n");
+            }
+
+            List<Contestant> contestants = databasePerson.FetchContestants();
+
+            Console.WriteLine("\nSkriv ut dykare:");
+            foreach (var contestant in contestants)
+            {
+                Console.Write(contestant.ID + ": " + contestant.GetFullName() + " " + contestant.Age + "\n");
+            }
+
             Console.ReadKey();
         }
     }
