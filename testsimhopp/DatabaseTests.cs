301129f1f5efe81b092ac8da33ad6464cfaa3e36
Tobias Lans <lanstobias@gmail.com>
2018-03-12 13:30:37 +0100

    Skapa DatabaseTests klassen

---
+++ testsimhopp/DatabaseTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simhopp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp.Tests
+{
+    [TestClass()]
+    public class DatabaseTests
+    {
+    }
+}
