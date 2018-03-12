13ec9e542cb1ed1afda45d8e10d61ed98352b997
Tobias Lans <lanstobias@gmail.com>
2018-03-12 13:29:28 +0100

    Skapa AuthTest klassen

---
+++ testsimhopp/AuthTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using Simhopp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Simhopp.Tests
+{
+    [TestClass()]
+    public class AuthTests
+    {
+    }
+}
