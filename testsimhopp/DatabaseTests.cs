a823cfa1cd9366434343a46978e707ac64ebdde7
Tobias Lans <lanstobias@gmail.com>
2018-03-12 13:31:04 +0100

    Skapa DatabaseTests.MailBelongsToOnePersonTest()

--- testsimhopp/DatabaseTests.cs
+++ testsimhopp/DatabaseTests.cs
@@ -11,5 +11,21 @@ namespace Simhopp.Tests
     [TestClass()]
     public class DatabaseTests
     {
+        [TestMethod()]
+        public void MailBelongsToOnePersonTest()
+        {
+            // Assign
+            Database database = new Database();
+            string givenEmail = "pelle@gmail.com";
+            string givenBadEmail = "noEmail@noemail.com";
+
+            // Act
+            bool shouldBeTrue = database.MailBelongsToOnePerson(givenEmail);
+            bool shouldBeFalse = database.MailBelongsToOnePerson(givenBadEmail);
+
+            // Assert
+            Assert.IsTrue(shouldBeTrue);
+            Assert.IsFalse(shouldBeFalse);
+        }
     }
 }
