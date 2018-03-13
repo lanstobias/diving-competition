1b3c524a241c9e5ca95ae7acf2243c2b5ca08eef
OSandh <oskarsandh@gmail.com>
2018-03-13 20:27:58 +0100

    Uppdatera LoginForm
    Nu behövs det email+lösenord som även kollas mot databasen

--- simhopp/LoginForm.Designer.cs
+++ simhopp/LoginForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Simhopp
+﻿using System;
+
+namespace Simhopp
 {
     partial class LoginForm
     {
@@ -29,12 +31,15 @@
         private void InitializeComponent()
         {
             this.buttonLogin = new System.Windows.Forms.Button();
-            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(95, 107);
+            this.buttonLogin.Location = new System.Drawing.Point(163, 97);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(75, 23);
             this.buttonLogin.TabIndex = 0;
@@ -42,19 +47,52 @@
             this.buttonLogin.UseVisualStyleBackColor = true;
             this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
-            // textBoxName
+            // tbUserName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(95, 66);
-            this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(100, 20);
-            this.textBoxName.TabIndex = 1;
+            this.tbUserName.Location = new System.Drawing.Point(95, 45);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(143, 20);
+            this.tbUserName.TabIndex = 1;
+            this.tbUserName.Click += new System.EventHandler(this.TbUserName_Click);
+            this.tbUserName.TextChanged += new System.EventHandler(this.TbUserName_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(95, 71);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(143, 20);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.UseSystemPasswordChar = true;
+            this.tbPassword.Click += new System.EventHandler(this.TbPassword_Click);
+            this.tbPassword.TextChanged += new System.EventHandler(this.TbPassword_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(12, 48);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(32, 13);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "Email";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 74);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(51, 13);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Lösenord";
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 179);
-            this.Controls.Add(this.textBoxName);
+            this.ClientSize = new System.Drawing.Size(251, 179);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
             this.Controls.Add(this.buttonLogin);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
@@ -66,6 +104,9 @@
         #endregion
 
         private System.Windows.Forms.Button buttonLogin;
-        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword;
     }
 }
--- simhopp/LoginForm.cs
+++ simhopp/LoginForm.cs
@@ -12,18 +12,100 @@ namespace Simhopp
 {
     public partial class LoginForm : Form
     {
-        public string JudgeName { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        public Judge Judge { get; set; }
+        
         public LoginForm()
         {
             InitializeComponent();
-            this.StartPosition = FormStartPosition.CenterParent;
+            StartPosition = FormStartPosition.CenterParent;
         }
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            JudgeName = this.textBoxName.Text;
-            this.DialogResult = DialogResult.OK;
-            this.Close();
+            Login();
+        }
+
+        /// <summary>
+        /// Try to login
+        /// </summary>
+        private void Login()
+        {
+            if (CheckDataInput.EmailCheckFormat(this.tbUserName.Text))
+            {
+                if (TryCredentials())
+                {
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+                else
+                {
+                    if (MessageBox.Show("Inlogg misslyckades, vill du försöka igen?", "", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                    {
+                        DialogResult = DialogResult.Cancel;
+                        Close();
+                    }
+                }
+            }
+            else
+            {
+                tbUserName.BackColor = Color.Red;
+            }
+        }
+
+        /// <summary>
+        /// Checks the given email and password against the database
+        /// </summary>
+        /// <returns>true if login is succesful</returns>
+        private bool TryCredentials()
+        {
+            tbUserName.BackColor = Color.White;
+
+            Email = tbUserName.Text;
+
+            Auth auth = new Auth();
+            Database db = new Database();
+
+            if (db.MailBelongsToOnePerson(Email))
+            {
+                string hashedPass = Helpers.SHA1Hash(tbPassword.Text);
+
+                string password = db.FetchPasswordFromEmail(Email);
+
+                if (auth.PasswordsMatch(hashedPass, password))
+                {
+                    foreach(var j in db.FetchJudges())
+                    {
+                        if (j.Email == Email)
+                        {
+                            Judge = j;
+                            break;
+                        }
+                    }
+                    return true;
+                }
+                else
+                    tbPassword.BackColor = Color.Red;
+                        
+            }
+            else
+            {
+                tbUserName.BackColor = Color.Red;
+                Email = "";
+            }
+
+            return false;
+        }
+
+        private void TbUserName_Click(object sender, EventArgs e)
+        {
+            tbUserName.BackColor = Color.White;
+        }
+
+        private void TbPassword_Click(object sender, EventArgs e)
+        {
+            tbPassword.BackColor = Color.White;
         }
     }
 }
