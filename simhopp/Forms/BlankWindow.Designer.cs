d971ce96ed463f00e6b6e47b54f6b9c008817f72
OSandh <oskarsandh@gmail.com>
2018-03-15 22:01:37 +0100

    Lägg till formen BlankWindow

---
+++ simhopp/Forms/BlankWindow.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Simhopp
+{
+    partial class BlankWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BlankWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 387);
+            this.Name = "BlankWindow";
+            this.Text = "BlankWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
